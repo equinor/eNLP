3463b980e0a3f816eb066a6737742886f7d22bad
cebirnie92 <cebirnie@gmail.com>
2019-11-25 23:33:23 +0100

    Changed docstring indentation of vader_sentiment_strength

--- enlp/understanding/sentiment.py
+++ enlp/understanding/sentiment.py
@@ -8,33 +8,33 @@ import pandas as pd
 def vader_sentiment_strength(textlist):
     """Compute sentiment strength of ENGLISH texts
 
-        Using the nltk implementation of the VADER approach devised by: Hutto, C.J. & Gilbert, E.E. (2014).
-        VADER: A Parsimonious Rule-based Model for Sentiment Analysis of Social Media Text. Eighth International
-        Conference on Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
+    Using the nltk implementation of the VADER approach devised by: Hutto, C.J. & Gilbert, E.E. (2014).
+    VADER: A Parsimonious Rule-based Model for Sentiment Analysis of Social Media Text. Eighth International
+    Conference on Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
 
-        Parameters
-        ----------
-        textlist : :obj:`list`
-            list of text strings, each of which are to be analysed
+    Parameters
+    ----------
+    textlist : :obj:`list`
+        list of text strings, each of which are to be analysed
 
-        Returns
-        -------
-            polarity_scores : :obj:`pandas.DataFrame`
-                Dataframe of the polarity score - positive values are positive valence, negative value are negative
-                valence. See cited paper for more details on scoring
+    Returns
+    -------
+        polarity_scores : :obj:`pandas.DataFrame`
+            Dataframe of the polarity score - positive values are positive valence, negative value are negative
+            valence. See cited paper for more details on scoring
 
 
-        Example
-        -------
-            >>> positive_text = 'I love your carefree nature.'
-            >>> negative_text = 'I abhor hostility.'
-            >>> textlist = [positive_text, negative_text]
-            >>> print (vader_sentiment_strength(textlist))
-            compound  negative  neutral  positive                          text
-            0    0.7845       0.0    0.225     0.775  I love your carefree nature.
-            1   -0.7579       1.0    0.000     0.000            I abhor hostility.
+    Example
+    -------
+        >>> positive_text = 'I love your carefree nature.'
+        >>> negative_text = 'I abhor hostility.'
+        >>> textlist = [positive_text, negative_text]
+        >>> print (vader_sentiment_strength(textlist))
+        compound  negative  neutral  positive                          text
+        0    0.7845       0.0    0.225     0.775  I love your carefree nature.
+        1   -0.7579       1.0    0.000     0.000            I abhor hostility.
 
-        """
+    """
     sid = SentimentIntensityAnalyzer()
 
     result_dicts = []
