e48da4f26db4ba177b0e9313bb87462ef9c357a9
cebirnie92 <cebirnie@gmail.com>
2019-11-27 21:35:40 +0100

    updated examples to include wordcloud creation

--- examples/ex_nlp_pipeline.py
+++ examples/ex_nlp_pipeline.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 plt.close('all') # very important for read the docs to avoid it crashing due to memory
 
 from enlp.processing.stdtools import get_stopwords, tokenise
-from enlp.pipeline import nlp_pipeline
+from enlp.pipeline import NLPPipeline
 from enlp.visualisation.freq_distribution import wordcloud_plot
 
 import spacy
@@ -18,7 +18,7 @@ import spacy
 
 ###############################################################################
 # Load Spacy's Norwegian language model and the example text
-#langmodel = spacy.load('nb_dep_ud_sm')
+langmodel = spacy.load('nb_dep_ud_sm')
 with open("example_data/no_den_stygge_andungen.txt", "r") as file:
     text = file.read()
 text = text.replace('\n', ' ')
@@ -36,23 +36,23 @@ all_stopwords, stopwords_nb, stopwords_en = get_stopwords()
 # - stem remaining words
 
 # Initialise object
-# processed_text = nlp_pipeline(langmodel, text)
-#
-# # Run processing as a pipeline
-# processed_text.rm_punctuation().rm_stopwords(stopwords=all_stopwords).nltk_stem_no()
-#
-# #############################################################################
-# # Compare text strings of firs 80 characters of the original and processed.
-#
-# print ('Original: ', text[:80], '...')
-# print ('Processed: ', processed_text.text[:80], '...')
-#
-#
-# #############################################################################
-# # Wordcloud comparison between most common words
-# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-# wordcloud_plot(tokenise(langmodel,text), ax=ax1)
-# wordcloud_plot(processed_text.tokenise().tokens, ax=ax2)
-# plt.tight_layout()
+processed_text = NLPPipeline(langmodel, text)
+
+# Run processing as a pipeline
+processed_text.rm_punctuation().rm_stopwords(stopwords=all_stopwords).nltk_stem_no()
+
+#############################################################################
+# Compare text strings of firs 80 characters of the original and processed.
+
+print ('Original: ', text[:80], '...')
+print ('Processed: ', processed_text.text[:80], '...')
+
+
+#############################################################################
+# Wordcloud comparison between most common words
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+wordcloud_plot(tokenise(langmodel,text), ax=ax1)
+wordcloud_plot(processed_text.tokenise().tokens, ax=ax2)
+plt.tight_layout()
 
 
