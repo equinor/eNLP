915a8c8d72b8354555db8224febe9bc7f36b46c5
Rafael Schlatter <rafaelschlatter@gmail.com>
2020-04-17 16:04:21 +0200

    Same import correction in test_get_stopwords.py.

--- tests/processing/test_get_stopwords.py
+++ tests/processing/test_get_stopwords.py
@@ -1,4 +1,6 @@
 import pytest
+from spacy.lang.en.stop_words import STOP_WORDS as STOP_WORDS_EN
+from spacy.lang.nb.stop_words import STOP_WORDS as STOP_WORDS_NB
 
 # Explicitly set path so don't need to run setup.py - if we have multiple copies of the code we would otherwise need
 # to setup a separate environment for each to ensure the code pointers are correct.
@@ -9,8 +11,7 @@ from enlp.processing.stdtools import get_stopwords
 
 def test_get_stopwords_english():
     # arrange - get stopwords outwith function
-    from spacy.lang.en.stop_words import STOP_WORDS
-    stops_en_direct = list(STOP_WORDS)
+    stops_en_direct = list(STOP_WORDS_EN)
     
     # act - get functions idea of stopwords
     stopwords_func, stopwords_nb_func, stopwords_en_func = get_stopwords()
@@ -21,8 +22,7 @@ def test_get_stopwords_english():
 
 def test_get_stopwords_norwegian():
     # arrange - get stopwords outwith function
-    from spacy.lang.nb.stop_words import STOP_WORDS
-    stops_nb_direct = list(STOP_WORDS)
+    stops_nb_direct = list(STOP_WORDS_NB)
 
     # act - get functions idea of stopwords
     stopwords_func, stopwords_nb_func, _ = get_stopwords()
@@ -33,11 +33,8 @@ def test_get_stopwords_norwegian():
 
 def test_get_stopwords_full():
     # arrange - get stopwords outwith function
-    from spacy.lang.en.stop_words import STOP_WORDS
-    stops_en_direct = list(STOP_WORDS)
-
-    from spacy.lang.nb.stop_words import STOP_WORDS
-    stops_nb_direct = list(STOP_WORDS)
+    stops_en_direct = list(STOP_WORDS_EN)
+    stops_nb_direct = list(STOP_WORDS_NB)
 
     stopwords = stops_en_direct + stops_nb_direct
     stopwords_direct = [str(i) for i in stopwords]
